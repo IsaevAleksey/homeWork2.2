7d6ecfcb3cff9d7173223c07a51ed14ca0f916d3
Alexey Isaev <montilarard@gmail.com>
2022-11-10 22:00:54 +0300

    homework completed

--- homeWork2.2/ViewController.swift
+++ homeWork2.2/ViewController.swift
@@ -14,12 +14,48 @@ class ViewController: UIViewController {
     
     @IBOutlet var changeSignalButtom: UIButton!
     
+    enum CurrentSignal {
+        case red
+        case yellow
+        case green
+    }
+    
+    private var currentSignal = CurrentSignal.red
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        redSignal.alpha = 0.3
+        yellowSignal.alpha = 0.3
+        greenSignal.alpha = 0.3
+        
+        changeSignalButtom.layer.cornerRadius = 10
+        redSignal.layer.cornerRadius = redSignal.frame.width / 2
+        yellowSignal.layer.cornerRadius = yellowSignal.frame.width / 2
+        greenSignal.layer.cornerRadius = greenSignal.frame.width / 2
     }
 
     @IBAction func changeSignalAction() {
+        if changeSignalButtom.currentTitle == "START" {
+            changeSignalButtom.setTitle("NEXT", for: .normal)
+        }
+        
+        switch currentSignal {
+        case .red:
+            redSignal.alpha = 1
+            yellowSignal.alpha = 0.3
+            greenSignal.alpha = 0.3
+            currentSignal = .yellow
+        case .yellow:
+            redSignal.alpha = 0.3
+            yellowSignal.alpha = 1
+            greenSignal.alpha = 0.3
+            currentSignal = .green
+        case .green:
+            redSignal.alpha = 0.3
+            yellowSignal.alpha = 0.3
+            greenSignal.alpha = 1
+            currentSignal = .red
+        }
     }
     
 }
